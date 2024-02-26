3d729b4a011793b90ddc74a5bf88c5536a6fa924
Denis Denisov <denis.denisov8701@gmail.com>
2024-02-26 20:00:51 +0100

    Initial Commit

---
+++ ContactsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsList
+//
+//  Created by Denis Denisov on 26/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
