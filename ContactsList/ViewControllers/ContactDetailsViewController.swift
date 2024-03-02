19c622a5ea114c24f0ab5f347dfa09d07a20719d
Denis Denisov <denis.denisov8701@gmail.com>
2024-03-02 13:56:52 +0100

    config for new setup en ContactDetailsVC

--- ContactsList/ViewControllers/ContactDetailsViewController.swift
+++ ContactsList/ViewControllers/ContactDetailsViewController.swift
@@ -18,14 +18,7 @@ final class ContactDetailsViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.title = person.fullName
-        
-        for contact in person.contacts {
-            switch contact {
-            case .phone(let number):
-                phoneLabel.text = number
-            case .email(let email):
-                emailLabel.text = email
-            }
-        }
+        phoneLabel.text = person.phone
+        emailLabel.text = person.email
     }
 }
